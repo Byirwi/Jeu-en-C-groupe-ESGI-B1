3389bda337f6c39a7718a5f823da0fffbcd42623
tania7777 <tania.ormeno@gmail.com>
2025-03-25 16:34:38 +0100

    Je viens d'ajouter des fonctionnalites pour choisir une difficulte, j'ajoutee de choix pour jouer avec l'ordi ou deux personnes et pour voir le score de chaqun. Bonne journee les gars

---
+++ cmake-build-debug/CMakeFiles/Projet_Group.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_Group.dir/project.c.obj"
+  "CMakeFiles/Projet_Group.dir/project.c.obj.d"
+  "Projet_Group.exe"
+  "Projet_Group.exe.manifest"
+  "Projet_Group.pdb"
+  "libProjet_Group.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_Group.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
